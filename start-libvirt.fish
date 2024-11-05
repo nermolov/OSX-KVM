da4b22d0c90de664471aed3e11784121221298fd
Taylor Ermolov <taylor@nermolov.com>
2024-11-05 12:51:12 -0500

    pause machine when console closed

--- start-libvirt.fish
+++ start-libvirt.fish
@@ -1,8 +1,18 @@
 #!/usr/bin/env fish
 
-# start system if not already booted
-virsh -c qemu:///system start macOS
+set CURRENT_STATE (virsh -c qemu:///system list --all | grep macOS | awk '{print $3}')
+
+if test $CURRENT_STATE = "paused"
+    virsh -c qemu:///system resume macOS
+else
+    virsh -c qemu:///system start macOS
+end
+
 # open console and block
 virt-manager --no-fork -c qemu:///system --show-domain-console macOS
-# once console is closed, gracefully shut down
-virsh -c qemu:///system shutdown macOS
+
+set CURRENT_STATE (virsh -c qemu:///system list --all | grep macOS | awk '{print $3}')
+# once console is closed, pause machine
+if test $CURRENT_STATE = "running"
+    virsh -c qemu:///system suspend macOS
+end
