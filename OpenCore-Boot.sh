b92e56d9254db43f422307b2c5be60e51254bc76
Taylor Ermolov <taylor@nermolov.com>
2024-11-05 12:51:12 -0500

    initial config with raw image

--- OpenCore-Boot.sh
+++ OpenCore-Boot.sh
@@ -56,7 +56,7 @@ args=(
   -device ide-hd,bus=sata.2,drive=OpenCoreBoot
   -device ide-hd,bus=sata.3,drive=InstallMedia
   -drive id=InstallMedia,if=none,file="$REPO_PATH/BaseSystem.img",format=raw
-  -drive id=MacHDD,if=none,file="$REPO_PATH/mac_hdd_ng.img",format=qcow2
+  -drive id=MacHDD,if=none,file="$REPO_PATH/mac_hdd_ng.img",format=raw
   -device ide-hd,bus=sata.4,drive=MacHDD
   # -netdev tap,id=net0,ifname=tap0,script=no,downscript=no -device virtio-net-pci,netdev=net0,id=net0,mac=52:54:00:c9:18:27
   -netdev user,id=net0,hostfwd=tcp::2222-:22 -device virtio-net-pci,netdev=net0,id=net0,mac=52:54:00:c9:18:27
